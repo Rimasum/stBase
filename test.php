d759688e035e349df22e10877b6f322567989a95
Masum Reza <mmr.masumreza@gmail.com>
2023-06-12 23:15:28 +0600

    update

--- test.php
+++ test.php
@@ -12,7 +12,7 @@
     <h1>This is Heading</h1>
     <?php
     $sArr = array("Home", "Services", "About", "Contact us", "Rayeed");
-    $link = array("https://www.google.com", "https://www.facebook.com", "/stbase/index.php", "Contact us", "Rayeed");
+    $link = array("https://www.google.com", "https://www.facebook.com", "/stbase/index", "Contact us", "Rayeed");
     // echo count($sArr);
     // $st = 'Students';
     // $list = '';
@@ -27,7 +27,7 @@
         <?php
         for ($i = 0; $i < count($sArr); $i++) {
         ?>
-            <li><a href="<?php echo $link[$i]; ?>"><?php echo $sArr[$i]; ?></a></li>
+            <li><a href="<?php echo $link[$i] . ".php"; ?>"><?php echo $sArr[$i]; ?></a></li>
         <?php
         }
         ?>
